30a664160b0185706491ca8233b0788901c4bbb5
Víctor M. Reyes <vm.reyesal@gmail.com>
2020-08-18 00:42:23 -0600

    Agregando diferentes operaciones matemáticas

--- main.js
+++ main.js
@@ -6,26 +6,43 @@ let values = Array.from(document.getElementsByTagName('td'));
 let sum = [];
 let total = [];
 let target = 0; 
-// let operations = []; 
+let operations = []; 
+let shifts = Math.ceil(values.length/3.6)
 
-let shifts = Math.ceil(values.length/2.8)
+values.map(o=>{
+    let getOperator = Math.floor(Math.random() * (4 - 1)) + 1
+    if(operations.length <= values.length/3.6){
+        operations.push(getOperator)
+    }
+})
 
-
-// values.map(o=>{
-//     let getOperator = Math.floor(Math.random() * (4 - 1)) + 1
-//     if(operations.length <= values.length/2.8){
-//         operations.push(getOperator)
-//     }
-// })
+let operation = (a, b, i)=>{
+    if (operations[i] == 1){
+        total.push(a+b)
+    } else if(operations[i] == 2){
+        total.push(a-b)
+    } else if(operations[i] == 3){
+        total.push(a*b)
+    } else if(operations[i] == 4){
+        total.push(a/b)
+    } 
+}
 
 let getNum = (e)=> {
     e.path[0].setAttribute('class', 'disabled')
     sum.push(parseInt(e.path[0].textContent))
     if (sum.length > 1){
-
-        document.getElementById('shifts').textContent =  shifts - (total.length +1) 
-
-        total.push(sum[0]+sum[1])
+        document.getElementById('shifts').textContent =  shifts - (total.length + 1) 
+        if (operations[shifts - (total.length+2)] == 1){
+            document.getElementById('operation').textContent = 'Suma'
+        } else if(operations[shifts - (total.length+2 )] == 2){
+            document.getElementById('operation').textContent = 'Resta'
+        } else if(operations[shifts - (total.length+2 )] == 3){
+            document.getElementById('operation').textContent = 'Multiplicación'
+        } else if(operations[shifts - (total.length+2 )] == 4){
+            document.getElementById('operation').textContent = 'División'
+        }
+        operation(sum[0], sum[1], shifts - (total.length + 1))
         let current = 0
         total.map(t=>{
             current += t
@@ -33,9 +50,7 @@ let getNum = (e)=> {
         })
         if (current == target){
             document.getElementById('result').innerText = '😎 ¡Ganaste!'
-
         }
-        
         else  if (shifts - total.length == 0){
             document.getElementById('result').innerText = '😔 ¡Perdiste!'
         }else if (current > target) {
@@ -57,5 +72,14 @@ document.getElementById('action').addEventListener('click', ()=>{
     location.reload();
 })
 
-//document.getElementById('operation').textContent = 'suma'
 document.getElementById('shifts').textContent = shifts
+
+if (operations[operations.length - 1] == 1){
+    document.getElementById('operation').textContent = 'Suma'
+} else if(operations[operations.length - 1] == 2){
+    document.getElementById('operation').textContent = 'Resta'
+} else if(operations[operations.length - 1] == 3){
+    document.getElementById('operation').textContent = 'Multiplicación'
+} else if(operations[operations.length - 1] == 4){
+    document.getElementById('operation').textContent = 'División'
+} 
