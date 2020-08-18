e0a3efaf9583cb812974ad0835b4253fb101ecda
Víctor M. Reyes <vm.reyesal@gmail.com>
2020-08-17 20:58:53 -0600

    Restringiendo turnos

--- main.js
+++ main.js
@@ -6,11 +6,25 @@ let values = Array.from(document.getElementsByTagName('td'));
 let sum = [];
 let total = [];
 let target = 0; 
+// let operations = []; 
+
+let shifts = Math.ceil(values.length/2.8)
+
+
+// values.map(o=>{
+//     let getOperator = Math.floor(Math.random() * (4 - 1)) + 1
+//     if(operations.length <= values.length/2.8){
+//         operations.push(getOperator)
+//     }
+// })
 
 let getNum = (e)=> {
     e.path[0].setAttribute('class', 'disabled')
     sum.push(parseInt(e.path[0].textContent))
     if (sum.length > 1){
+
+        document.getElementById('shifts').textContent =  shifts - (total.length +1) 
+
         total.push(sum[0]+sum[1])
         let current = 0
         total.map(t=>{
@@ -20,7 +34,11 @@ let getNum = (e)=> {
         if (current == target){
             document.getElementById('result').innerText = '😎 ¡Ganaste!'
 
-        } else if (current > target) {
+        }
+        
+        else  if (shifts - total.length == 0){
+            document.getElementById('result').innerText = '😔 ¡Perdiste!'
+        }else if (current > target) {
             document.getElementById('result').innerText = '😔 ¡Perdiste!'
         }
         sum.shift()
@@ -38,3 +56,6 @@ document.getElementById('target').innerText = target
 document.getElementById('action').addEventListener('click', ()=>{
     location.reload();
 })
+
+//document.getElementById('operation').textContent = 'suma'
+document.getElementById('shifts').textContent = shifts
